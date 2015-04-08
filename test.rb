278423376a99e8f53c70bfe3479f8461fc63cc62
hedamin <hedm0423@163.com>
2015-04-08 20:09:57 +0800

    final initial

---
+++ test.rb
@@ -0,0 +1,13 @@
+modification second.test.rb
+<<<<<<< HEAD
+fourth modifcation fourth.test.rb
+fifth modification fifth.test.rb
+=======
+sixth modification seven.test.rb
+seventh modification seven.test.rb
+>>>>>>> master
+modification second.test.rb
+fourth modification fourth
+fifth modification fifth
+sixth modification six
+seventh modification seven
